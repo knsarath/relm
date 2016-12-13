447eb66769f750dc85bb9991c9c6254b38e93723
sarath.kn <sarath.kn@robosoftin.com>
2016-12-13 20:40:18 +0530

    changed to new version and changed implementation

---
+++ app/src/main/java/com/test/rlm/activity/DummyData.java
@@ -0,0 +1,42 @@
+package com.test.rlm.activity;
+
+import com.test.rlm.model.Book;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sarath on 13/12/16.
+ */
+public class DummyData {
+    public static ArrayList<Book> getDummyBooks() {
+        ArrayList<Book> books = new ArrayList<>();
+        Book book = new Book();
+        book.setId(1);
+        book.setAuthor("Reto Meier");
+        book.setTitle("Android 4 Application Development");
+        book.setImageUrl("http://api.androidhive.info/images/realm/1.png");
+        books.add(book);
+
+        book = new Book();
+        book.setId((int) (2 + System.currentTimeMillis()));
+        book.setAuthor("Itzik Ben-Gan");
+        book.setTitle("Microsoft SQL Server 2012 T-SQL Fundamentals");
+        book.setImageUrl("http://api.androidhive.info/images/realm/2.png");
+        books.add(book);
+
+        book = new Book();
+        book.setId((int) (3 + System.currentTimeMillis()));
+        book.setAuthor("Magnus Lie Hetland");
+        book.setTitle("Beginning Python: From Novice To Professional Paperback");
+        book.setImageUrl("http://api.androidhive.info/images/realm/3.png");
+        books.add(book);
+
+        book = new Book();
+        book.setId((int) (4 + System.currentTimeMillis()));
+        book.setAuthor("Chad Fowler");
+        book.setTitle("The Passionate Programmer: Creating a Remarkable Career in Software Development");
+        book.setImageUrl("http://api.androidhive.info/images/realm/4.png");
+        books.add(book);
+        return books;
+    }
+}
--- app/src/main/java/com/test/rlm/activity/MainActivity.java
+++ app/src/main/java/com/test/rlm/activity/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -23,10 +22,10 @@ import app.androidhive.info.realm.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private BooksAdapter adapter;
-    private LayoutInflater inflater;
     private FloatingActionButton fab;
     private RecyclerView recycler;
     private RealmController mRealmController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +39,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //set toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         setupRecycler();
         setRealmData();
-
-        // refresh the realm instance
-        //RealmController.with(this).refresh();
-        // get all persisted objects
-        // create the helper adapter and notify data set changes
-        // changes will be reflected automatically
 
         Toast.makeText(this, "Press card item for edit, long press to remove item", Toast.LENGTH_LONG).show();
 
@@ -57,11 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private void setupRecycler() {
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
         recycler.setHasFixedSize(true);
-
-        // use a linear layout manager since the cards are vertically scrollable
         final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recycler.setLayoutManager(layoutManager);
@@ -72,51 +62,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setRealmData() {
-        ArrayList<Book> books = new ArrayList<>();
-        Book book = new Book();
-        book.setId(1);
-        book.setAuthor("Reto Meier");
-        book.setTitle("Android 4 Application Development");
-        book.setImageUrl("http://api.androidhive.info/images/realm/1.png");
-        books.add(book);
-        mRealmController.saveAllOrUpdate(books);
+        final ArrayList<Book> dummyBooks = DummyData.getDummyBooks();
+        mRealmController.saveAllOrUpdate(dummyBooks);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fab:
-                inflater = MainActivity.this.getLayoutInflater();
-                View content = inflater.inflate(R.layout.edit_item, null);
+                View content = getLayoutInflater().inflate(R.layout.edit_item, null);
                 final EditText editTitle = (EditText) content.findViewById(R.id.title);
                 final EditText editAuthor = (EditText) content.findViewById(R.id.author);
                 final EditText editThumbnail = (EditText) content.findViewById(R.id.thumbnail);
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setView(content)
-                        .setTitle("Add book")
-                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
+                final DialogInterface.OnClickListener OkClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final Book book = new Book();
+                        book.setId(mRealmController.getBooks().size() + 1);
+                        book.setTitle(editTitle.getText().toString());
+                        book.setAuthor(editAuthor.getText().toString());
+                        book.setImageUrl(editThumbnail.getText().toString());
 
-                                final Book book = new Book();
-                                //book.setId(RealmController.getInstance().getBooks().size() + 1);
-                                book.setId(mRealmController.getBooks().size());
-                                book.setTitle(editTitle.getText().toString());
-                                book.setAuthor(editAuthor.getText().toString());
-                                book.setImageUrl(editThumbnail.getText().toString());
+                        if (editTitle.getText() == null || editTitle.getText().toString().equals("") || editTitle.getText().toString().equals(" ")) {
+                            Toast.makeText(MainActivity.this, "Entry not saved, missing title", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Persist your data easily
+                            mRealmController.save(book);
+                            adapter.notifyDataSetChanged();
+                            recycler.scrollToPosition(mRealmController.getBooks().size() - 1);
 
-                                if (editTitle.getText() == null || editTitle.getText().toString().equals("") || editTitle.getText().toString().equals(" ")) {
-                                    Toast.makeText(MainActivity.this, "Entry not saved, missing title", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    // Persist your data easily
-                                    mRealmController.save(book);
-                                    adapter.notifyDataSetChanged();
-                                    recycler.scrollToPosition(mRealmController.getBooks().size() - 1);
-
-                                    /**
-                                     * Tried to do the same in background thread
-                                     */
+                            /**
+                             * Tried to do the same in background thread
+                             */
                                    /* new AsyncTask<Void, Void, Void>() {
                                         @Override
                                         protected Void doInBackground(Void... params) {
@@ -132,19 +110,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                         }
                                     }.execute();*/
 
-                                }
-                            }
-                        })
-                        .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                        }
 
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        });
+                    }
+
+                };
+                final DialogInterface.OnClickListener cancelClickListener = new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setView(content)
+                        .setTitle("Add book")
+                        .setPositiveButton(android.R.string.ok, OkClickListener)
+                        .setNegativeButton(android.R.string.cancel, cancelClickListener);
                 AlertDialog dialog = builder.create();
                 dialog.show();
                 break;
         }
     }
+
+
 }
